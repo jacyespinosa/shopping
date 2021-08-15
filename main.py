ec9e8845090303830c90b91aa86c5db0fefb0b37
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-08-15 16:35:30 -0700

    Incorporated Bootstrap by setting up the header.html

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
+
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1000, debug=True)
